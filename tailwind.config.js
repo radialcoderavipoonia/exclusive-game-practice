5f7ee4695f3274df721089d0e21f55471af569a0
radialcoderavipoonia <ravipoonia28122002@gmail.com>
2024-02-14 09:55:21 +0530

    configuration edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,9 +45,6 @@ module.exports = {
         "bermuda":"#6AE3A9",
       },
       fontSize: {
-        sm: "14px",
-        base: "16px",
-        xl: "20px",
         "2xl": "32px",
         "3xl": "48px",
         "4xl": "72px",
